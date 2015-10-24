34cddab3804cf0c3d13ebc2571c718d93cff41fc
Rouven Alexander Rieker <therouv@googlemail.com>
2015-10-24 22:05:25 +0200

    [FEATURE] Added imprint fields to store configuration + changed templates

---
+++ Block/Imprint/Content.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Copyright © 2015 FireGento e.V. - All rights reserved.
+ * See LICENSE.md bundled with this module for license details.
+ */
+namespace FireGento\MageSetup\Block\Imprint;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+/**
+ * Class Content
+ *
+ * @package FireGento\MageSetup\Block\Imprint
+ */
+class Content extends \Magento\Framework\View\Element\Template
+{
+    const XML_PATH_IMPRINT = 'general/imprint/';
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param ScopeConfigInterface                             $scopeConfig
+     * @param array                                            $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        ScopeConfigInterface $scopeConfig,
+        array $data = []
+    )
+    {
+        parent::__construct($context, $data);
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * Retrieve the specific country name by the selected country code
+     *
+     * @return string Country
+     */
+    public function getCountry()
+    {
+        $countryCode = $this->getImprintValue('country');
+
+        // TODO: Get country name
+
+        return $countryCode;
+    }
+
+    /**
+     * Retrieve the setting "website". If parameter checkForProtocol is true,
+     * check if there is a valid protocol given, otherwise add http:// manually.
+     *
+     * @param  bool $checkForProtocol Flag if website url should be checked for http(s) protocol
+     * @return string Website URL
+     */
+    public function getWeb($checkForProtocol = false)
+    {
+        $web = $this->getImprintValue('web');
+        if ($checkForProtocol && strlen(trim($web))) {
+            if (strpos($web, 'http://') === false
+                && strpos($web, 'https://') === false
+            ) {
+                $web = 'http://' . $web;
+            }
+        }
+
+        return $web;
+    }
+
+    /**
+     * Try to limit spam by generating a javascript email link
+     *
+     * @param boolean true
+     * @return string
+     */
+    public function getEmail($antispam = false)
+    {
+        $email = $this->getImprintValue('email');
+        $parts = explode('@', $email);
+
+        if (!$antispam) {
+            return $email;
+        }
+
+        if (count($parts) == 0) {
+            return;
+        }
+
+        $html = '<a href="#" onclick="javascript:toRecipient();">';
+        $html .= $parts[0] . '<span class="no-display">nospamplease</span>@<span class="no-display">nospamplease</span>' . $parts[1];
+        $html .= '</a>';
+        $html .= $this->getEmailJs($parts);
+
+        return $html;
+    }
+
+    /**
+     * Generate JS code
+     *
+     * @param $parts
+     * @return string
+     */
+    public function getEmailJs($parts)
+    {
+        $js = <<<JS
+<script>function toRecipient(){var m = '$parts[0]';m += '@';m += '$parts[1]';location.href= "mailto:"+m;}</script>
+JS;
+
+        return $js;
+    }
+
+    /**
+     * @param string $field
+     * @return mixed
+     */
+    public function getImprintValue($field)
+    {
+        return $this->scopeConfig->getValue(self::XML_PATH_IMPRINT . $field);
+    }
+}
