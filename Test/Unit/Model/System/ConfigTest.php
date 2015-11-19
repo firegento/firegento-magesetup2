0ed48195a5e78f6263ca3e47fc4b1b942c298d83
Matthias Walter <m.walter@netz98.de>
2015-11-19 17:13:43 +0100

    fix system config test

--- Test/Unit/Model/System/ConfigTest.php
+++ Test/Unit/Model/System/ConfigTest.php
@@ -5,12 +5,14 @@
  */
 namespace FireGento\MageSetup\Test\Unit\Model\System;
 
+use Magento\Framework\TestFramework\Unit\BaseTestCase;
+
 /**
  * Class Config
  *
  * @package FireGento\MageSetup\Test\Unit\Model\System
  */
-class Config extends \PHPUnit_Framework_TestCase
+class Config extends BaseTestCase
 {
     /**
      * @var \FireGento\MageSetup\Model\Config
@@ -21,10 +23,22 @@ class Config extends \PHPUnit_Framework_TestCase
     {
         parent::setUp();
 
-        $scopeConfigMock = $this->getMock('\Magento\Framework\App\Config\ScopeConfigInterface');
-        $scopeConfigMock->expects($this->any())->method('getValue')->with($this->equalTo('general/country/eu_countries'))->will($this->returnValue('DE,AT'));
+        $scopeConfigMock = $this->getMock(\Magento\Framework\App\Config\ScopeConfigInterface::class);
+        $scopeConfigMock->expects($this->any())->method('getValue')
+            ->with($this->equalTo('general/country/eu_countries'))
+            ->will($this->returnValue('DE,AT'));
 
-        $this->config = new \FireGento\MageSetup\Model\System\Config($scopeConfigMock);
+        $context = $this->getMock(\Magento\Framework\App\Helper\Context::class, [], [], '', false);
+        $context->expects($this->any())->method('getScopeConfig')->will($this->returnValue($scopeConfigMock));
+        /** @var \Magento\Framework\App\Helper\Context $context */
+
+        // $this->config = new \FireGento\MageSetup\Model\System\Config($context);
+        $this->config = $this->objectManager->getObject(
+            \FireGento\MageSetup\Model\System\Config::class,
+            [
+                'context' => $context
+            ]
+        );
     }
 
     /**
