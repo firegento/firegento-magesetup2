d5c729ee24b88ade363da08d6a3d6fc7b22359f2
Rouven Alexander Rieker <therouv@googlemail.com>
2016-07-02 18:38:33 +0200

    [FEATURE] #23 Add visible checkout attributes to frontend view (cart/checkout)

---
+++ Service/GetVisibleCheckoutAttributesServiceInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright © 2015 FireGento e.V. - All rights reserved.
+ * See LICENSE.md bundled with this module for license details.
+ */
+namespace FireGento\MageSetup\Service;
+
+/**
+ * Interface GetVisibleCheckoutAttributesServiceInterface
+ *
+ * @package FireGento\MageSetup\Service
+ */
+interface GetVisibleCheckoutAttributesServiceInterface
+{
+    /**
+     * @return array|bool
+     */
+    public function execute();
+}
