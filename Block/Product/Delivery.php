4ff3d6d6c94faf62baf3823ab28f67c1962af920
Taras Demianchenko <demtarico@gmail.com>
2016-07-03 14:19:08 +0200

    [FEATURE] #24 Delivery Time //Output attribute on product page

--- Block/Product/Delivery.php
+++ Block/Product/Delivery.php
@@ -8,20 +8,12 @@ namespace FireGento\MageSetup\Block\Product;
 class Delivery extends \Magento\Catalog\Block\Product\View\Description
 {
     /**
-     * @var \Magento\Catalog\Model\Product
-     */
-    protected $_product = null;
-
-    /**
      * Retrieve current product object
      *
      * @return \Magento\Catalog\Model\Product
      */
     public function getProduct()
     {
-        if (!$this->_product) {
-            $this->_product = $this->getData('product');
-        }
-        return $this->_product;
+        return $this->getData('product');;
     }
 }
