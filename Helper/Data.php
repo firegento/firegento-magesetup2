34e2255c58cafdff0056d9ffa3160efb250459b4
Roman Hutterer <roman.hutterer@copex.io>
2018-11-19 16:15:08 +0100

    respect setting if config display_block_below_price is set to yes or no https://github.com/firegento/firegento-magesetup2/issues/103

---
+++ Helper/Data.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright © 2018 FireGento e.V.
+ * See LICENSE.md bundled with this module for license details.
+ */
+
+namespace FireGento\MageSetup\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
+
+/**
+ * Class AroundRenderPlugin
+ * @package FireGento\MageSetup\Helper
+ */
+class Data extends AbstractHelper
+{
+    public function getConfigValue($field, $storeId = null)
+    {
+        return $this->scopeConfig->getValue(
+            $field, ScopeInterface::SCOPE_STORE, $storeId
+        );
+    }
+}
--- Plugin/Pricing/AroundRenderPlugin.php
+++ Plugin/Pricing/AroundRenderPlugin.php
@@ -3,26 +3,37 @@
  * Copyright © 2016 FireGento e.V.
  * See LICENSE.md bundled with this module for license details.
  */
+
 namespace FireGento\MageSetup\Plugin\Pricing;
 
 /**
  * Class AroundRenderPlugin
- *
  * @package FireGento\MageSetup\Model\Plugin
  */
 class AroundRenderPlugin
 {
+    /** @const settings string to enable the block below price */
+    const ENABLED_DISPLAY_BELOW_PRICE_XML = 'catalog/price/display_block_below_price';
+
     /**
      * @var \Magento\Framework\App\Config\ScopeConfigInterface
      */
     protected $_scopeConfig;
 
     /**
+     * @var \FireGento\MageSetup\Helper\Data
+     */
+    protected $_helper;
+
+    /**
      * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
      */
-    public function __construct(\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig)
-    {
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \FireGento\MageSetup\Helper\Data $helper
+    ) {
         $this->_scopeConfig = $scopeConfig;
+        $this->_helper = $helper;
     }
 
     /**
@@ -42,6 +53,9 @@ class AroundRenderPlugin
         array $arguments = []
     ) {
         $returnValue = $proceed($priceCode, $saleableItem, $arguments);
+        if (!$this->_helper->getConfigValue(self::ENABLED_DISPLAY_BELOW_PRICE_XML)) {
+            return $returnValue;
+        }
         if (trim($returnValue) != '') {
             $block = $subject->getLayout()->getBlock('magesetup.product.price.details');
             if ($block) {
