a4c5318df7b2c026191aa1243828f2f3a36819f6
Jens Richter <j.richter@netz98.de>
2018-12-20 12:26:11 +0100

    [change] use php7.1 for the unit tests with pretty output

--- Test/script/travis_script.sh
+++ Test/script/travis_script.sh
@@ -3,7 +3,13 @@
 set -e
 trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
 
-if [ "$deps" == "cs-phpunit" ]; then
+if [ "$TEST_SUITE" == "unit_cs" ]; then
+
+    echo "Check EcgM2 standard"
+    echo "####################"
     vendor/bin/phpcs -p -n --colors --extensions=php,phtml --standard=vendor/magento-ecg/coding-standard/EcgM2 --ignore=./vendor,/Test $TRAVIS_BUILD_DIR
-    vendor/bin/phpunit  --testdox --colors=always --debug
+
+    echo "Run unit tests"
+    echo "###############"
+    vendor/bin/phpunit
 fi
