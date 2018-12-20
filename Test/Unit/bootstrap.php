4aa55acaea6f0a4b1bf5576c08de40c65e388aba
Jens Richter <j.richter@netz98.de>
2018-12-20 10:31:19 +0100

    [change] add phpunit config

---
+++ Test/Unit/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Copyright © 2016 FireGento e.V. - All rights reserved.
+ * See LICENSE.md bundled with this module for license details.
+ */
+require_once realpath(__DIR__ . '/../../vendor/autoload.php');
--- Test/script/travis_script.sh
+++ Test/script/travis_script.sh
@@ -3,8 +3,8 @@
 set -e
 trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
 
-if [ "$deps" == "codeSniffer" ]; then
-    vendor/bin/phpcs -p -n --colors --extensions=php,phtml --standard=vendor/magento-ecg/coding-standard/EcgM2 $TRAVIS_BUILD_DIR
+if [ "$deps" == "cs-phpunit" ]; then
+    vendor/bin/phpcs -p -n --colors --extensions=php,phtml --standard=vendor/magento-ecg/coding-standard/EcgM2 --ignore=./vendor,/Test $TRAVIS_BUILD_DIR
 fi
 
 if [ "$deps" == "high" ]; then
