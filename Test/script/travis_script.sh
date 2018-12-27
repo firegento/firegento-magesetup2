2c48b04c48cc2e3f514c7ab2a1df801a6d2f95c2
Jens Richter <j.richter@netz98.de>
2018-12-27 17:41:34 +0100

    [change] fix issue this test library

--- Test/script/travis_script.sh
+++ Test/script/travis_script.sh
@@ -5,17 +5,27 @@
 set -e
 trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
 
-if [ "$TEST_SUITE" == "unit_cs" ]; then
+if [ "$CODE_QUALITY" == "true" ]; then
 
-    echo "Check EcgM2 standard"
+    echo "Check code quality"
     echo "####################"
+
+    composer require "magento-ecg/coding-standard": "^3.0"
+
+    echo "- checking ecgM2"
+
     $MAGENTO_ROOT/vendor/bin/phpcs -p -n --colors --extensions=php,phtml --standard=vendor/magento-ecg/coding-standard/EcgM2 --ignore=./vendor,/Test $TRAVIS_BUILD_DIR
+
+    echo "- checking php-cs"
 
     $MAGENTO_ROOT/vendor/bin/php-cs-fixer fix --config=.php_cs.dist --dry-run --diff $TRAVIS_BUILD_DIR
 
-    ${MAGENTO_ROOT}vendor/bin/phpcs -p --colors --extensions=php/php --standard=dev/tests/static/framework/Magento/ $MAGENTO_ROOT/vendor/firegento/magesetup2/
+    echo "- checking magento cs"
+    $MAGENTO_ROOT/vendor/bin/phpcs -p --colors --extensions=php/php --standard=dev/tests/static/framework/Magento/ $TRAVIS_BUILD_DIR
 
-    echo "Run unit tests"
-    echo "###############"
-    vendor/bin/phpunit
 fi
+
+echo "Run unit tests"
+echo "###############"
+
+php bin/magento dev:tests:run unit
