d94d77660090e0c799f2b4a328abb8f3c9a10e34
Jens Richter <j.richter@netz98.de>
2018-12-27 18:09:41 +0100

    [change] fix path issue

--- Test/script/travis_script.sh
+++ Test/script/travis_script.sh
@@ -10,22 +10,23 @@ if [ "$CODE_QUALITY" == "true" ]; then
     echo "Check code quality"
     echo "####################"
 
+    cd $MAGENTO_ROOT
+
     composer require "magento-ecg/coding-standard":"^3.0"
 
     echo "- checking ecgM2"
-
-    $MAGENTO_ROOT/vendor/bin/phpcs -p -n --colors --extensions=php,phtml --standard=vendor/magento-ecg/coding-standard/EcgM2 --ignore=./vendor,/Test $TRAVIS_BUILD_DIR
+    vendor/bin/phpcs -p -n --colors --extensions=php,phtml --standard=vendor/magento-ecg/coding-standard/EcgM2 --ignore=./vendor,/Test $TRAVIS_BUILD_DIR
 
     echo "- checking php-cs"
 
-    $MAGENTO_ROOT/vendor/bin/php-cs-fixer fix --config=.php_cs.dist --dry-run --diff $TRAVIS_BUILD_DIR
+    vendor/bin/php-cs-fixer fix --config=.php_cs.dist --dry-run --diff $TRAVIS_BUILD_DIR
 
     echo "- checking magento cs"
-    $MAGENTO_ROOT/vendor/bin/phpcs -p --colors --extensions=php/php --standard=dev/tests/static/framework/Magento/ $TRAVIS_BUILD_DIR
+    vendor/bin/phpcs -p --colors --extensions=php/php --standard=dev/tests/static/framework/Magento/ $TRAVIS_BUILD_DIR
 
 fi
 
 echo "Run unit tests"
 echo "###############"
-cp $TRAVIS_BUILD_DIR/phpunit.unittest.xml $MAGENTO_ROOT/dev/tests/unit/phpunit.xml
+cp ${TRAVIS_BUILD_DIR}/phpunit.unittest.xml dev/tests/unit/phpunit.xml
 php bin/magento dev:tests:run unit
